37e90240aff6906ce2aeff0c4b53bb78d4e71281
chetan.goudar@tibilsolutions.com <sdevaraja85@gmail.com>
2021-01-15 09:28:08 +0530

    "Added the project"

---
+++ PageObjects/AdminLoginPage.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+class AdminLogin:
+    selectbox_selectuser_id="userType"
+    text_box_username_id="username"
+    text_box_passsword_id="password"
+    button_login_xpath="//*[@id='app']/div/div/div/div/div/button"
+
+    # Admin AddUser Locators
+
+    #Admin UserList Locators
+
+    header_userlist_xpath = "//*[@id='primary-content']/div/div[1]/div/div/h3"
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def setSelectUser(self,select_username):
+        select = Select(self.driver.find_element_by_id(self.selectbox_selectuser_id))
+        select.select_by_visible_text(select_username)
+
+    def setUserName(self,username):
+        self.driver.find_element_by_id(self.text_box_username_id).clear()
+        self.driver.find_element_by_id(self.text_box_username_id).send_keys(username)
+
+    def setPassword(self,password):
+        self.driver.find_element_by_id(self.text_box_passsword_id).clear()
+        self.driver.find_element_by_id(self.text_box_passsword_id).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.button_login_xpath).click()
+
+    #Admin UserList Methods
+    def getUserListPage(self):
+        txt = self.driver.find_element_by_xpath(self.header_userlist_xpath).text
+        return txt
+
+
+    #Admin AddUser Methods
+
+
+
---
+++ TestCases/test_admin_login.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from utilities.readProperties import ReadConfig
+from PageObjects.AdminLoginPage import AdminLogin
+from utilities.customLogger import LogGen
+class Test_Admin_Login:
+
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUsername()
+    password = ReadConfig.getPassword()
+    logger = LogGen.logen()
+
+    def test_admin_login_page(self, setup):
+        self.logger.info("********************Test Admin Login Started********************************")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        actual_title = self.driver.title
+        if actual_title == "Atria WindPower":
+            assert True
+            self.driver.close()
+            self.logger.info("********************Test Admin Login Ended********************************")
+        else:
+            self.driver.save_screenshot("../Screenshots/"+"test_login_title.png")
+            self.driver.close()
+            self.logger.info("********************Test Admin Login Ended Due to difference in the title********************************")
+            assert False
+
+    # def test_admin_login_with_valid_username_and_valid_password(self,setup):
+    #     self.logger.info("********************Test Admin Login With Valid Username And Password Started********************************")
+    #     self.driver = setup
+    #     self.driver.get(self.baseURL)
+    #     self.admin_login = AdminLogin(self.driver)
+    #     self.admin_login.setSelectUser("Admin")
+    #     self.admin_login.setUserName(ReadConfig.getUsername())
+    #     self.admin_login.setPassword(ReadConfig.getPassword())
+    #     self.admin_login.clickLogin()
+    #     if self.admin_login.getUserListPage() == "Users List":
+    #         assert True
+    #         self.logger.info(
+    #             "********************Test Admin Login With Valid Username And Password successfully login********************************")
+    #     else:
+    #         self.driver.save_screenshot("../Screenshots/"+"test_admin_login_with_valid_username_and_valid_password.png")
+    #         self.logger.info(
+    #             "********************Test Admin Login With Valid Username And Password Failed********************************")
+    #         self.driver.close()
+    #         assert False
+
+
+
+
+
+
